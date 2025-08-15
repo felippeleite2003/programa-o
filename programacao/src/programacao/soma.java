d98e774d7784483e7e448b3bac43cee1fda08db2
felippeleite2003 <felippeleite354@gmail.com>
2025-08-15 12:09:41 -0300

    Primeiro commit

---
+++ programacao/src/programacao/soma.java
@@ -0,0 +1,24 @@
+package programacao;
+
+import java.util.Scanner;
+
+public class soma {
+
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 
+	 int x = sc.nextInt();
+	 int y = sc.nextInt();
+	 
+	 int resultado = x + y;
+	 
+	 System.out.println("SOMA = " + resultado);
+	 
+	 sc.close();
+	 
+		
+		
+
+	}
+
+}
